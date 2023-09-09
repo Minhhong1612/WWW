ba29e9c19ed3cd8b056b0033a47a48224bfbc803
Minh Hong <minhhong1612@gmail.com>
2023-09-09 08:56:38 +0700

    update

---
+++ src/main/java/vn/edu/iuh/fit/tuan01/ControlServlet.java
@@ -0,0 +1,22 @@
+package vn.edu.iuh.fit.tuan01;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "ControlServlet",value = "/ControlServlet")
+public class ControlServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
